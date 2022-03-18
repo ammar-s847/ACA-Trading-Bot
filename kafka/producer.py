ec1e3580c8b60ff0ddc18b30a522983510f09bba
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2022-03-17 23:41:38 -0400

    added initial data producer for training data

--- kafka/producer.py
+++ kafka/producer.py
@@ -6,14 +6,15 @@ from config import AV_API_KEY, KAFKA_HOST, KAFKA_PORT, KAFKA_TOPICS
 from scripts.eth_hourly import eth_hourly
 
 '''
+ETH-Hourly message format:
+{
+    "format": "hour", "initial" (hour is a single point, initial is the full data used for training)
+    "data": <Time-series message>
+}
+
 Time-series message format:
 {
-    "datetime"
-    "open"
-    "high"
-    "low"
-    "close"
-    "volume"
+    "datetime", "open", "high", "low", "close", "volume"
 }
 '''
 
@@ -25,9 +26,23 @@ producer = KafkaProducer(
     value_serializer=serializer
 )
 
+load_initial_eth_hourly = False
+
 if __name__ == "__main__":
+    if load_initial_eth_hourly:
+        producer.send(
+            KAFKA_TOPICS['eth-hourly'], 
+            {
+                "format": "hour",
+                "data": eth_hourly(AV_API_KEY)
+            }
+        )
+
     schedule.every(1).hours.do(
         producer.send, 
         topic = KAFKA_TOPICS['eth-hourly'], 
-        value = eth_hourly(AV_API_KEY)
+        value = {
+            "format": "hour",
+            "data": eth_hourly(AV_API_KEY)
+        }
     )
--- kafka/scripts/eth_hourly.py
+++ kafka/scripts/eth_hourly.py
@@ -16,3 +16,19 @@ def eth_hourly(api_key: str) -> dict:
     
 def eth_hourly_full(api_key: str) -> list:
     url = f"https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=60min&outputsize=full&apikey={api_key}"
+
+    res = requests.get(url).json()
+    ts_list = res['Time Series Crypto (60min)']
+
+    full_data = []
+    
+    for k in ts_list.keys():
+        hour_data = ts_list[k]
+        hour_data_new = dict()
+        for i in hour_data.keys():
+            hour_data_new[i[3:]] = hour_data[i]
+        hour_data = hour_data_new
+        hour_data['datetime'] = k
+        full_data.append(hour_data)
+
+    return full_data
